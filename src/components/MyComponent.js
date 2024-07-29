0330e83d472c89fc1a6f84a54b4a0589a9ecb3a0
khang <khangcaopham2003@gmail.com>
2024-07-29 10:14:58 +0700

    DOM learn

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,12 +5,23 @@ class MyComponent extends React.Component{
         email: 'Khang@it.edu.vn',
         age : 21
     }
+
+    handleHover = (event) => {
+        console.log('My name is hover')
+    }
+
+    handleClick = (event) => {
+        console.log('My name is')
+    }
     //JSX
     render(){
         return(
             <div>
                 <h1>Hello World From MyComponent</h1>
                 <p>name = {this.state.name}</p>
+                <button onClick={this.handleClick}>Click button</button>
+                <button onMouseOver={this.handleHover}>Hover button</button>
+
             </div>
         );
     }
