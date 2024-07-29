076c805257589c9973f50cb62b41aa793d2c591c
khang <khangcaopham2003@gmail.com>
2024-07-29 21:55:06 +0700

    conditional output learn

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,21 +1,32 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
-    render() {
-        const { listUser } = this.props
-        return (
-            <div>
-                {listUser.map((user)=>{
-                    return(
-                        <div key={user.id}>
-                            <p>Name: {user.name}</p>
-                            <p>Age: {user.age}</p>
-                           <hr/>
-                        </div>
-                    )
-                })}
-            </div>
-        );
+    state ={
+        showList : true
     }
+
+  render() {
+    const { listUser } = this.props;
+    return (
+      <div>
+        <span>
+          <button onClick={() => this.setState({ showList: !this.state.showList })}>Show user infor</button>
+        </span>
+        {this.state.showList &&
+          <div>
+            {listUser.map((user) => {
+              return (
+                <div className={user.age < 18 ? "green" : "red"} key={user.id}>
+                  <p>Name: {user.name}</p>
+                  <p>Age: {user.age}</p>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        }
+      </div>
+    );
+  }
 }
-export default DisplayInfor
+export default DisplayInfor;
