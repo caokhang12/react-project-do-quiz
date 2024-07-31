655e86915e451212a839f970988e08ae959a3799
khang <khangcaopham2003@gmail.com>
2024-07-31 22:52:44 +0700

    stateless/full learn

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,51 +2,52 @@ import React from "react";
 import "./DisplayInfor.scss";
 import logo from "./../logo.svg";
 
-class DisplayInfor extends React.Component {
-  state = {
-    showList: true,
-  }; 
+// class DisplayInfor extends React.Component {
+//   render() {
+//     const { listUser } = this.props;
+//     return (
+//       <div className="display-infor">
+//         {true && (
+//           <div>
+//             {listUser.map((user) => {
+//               return (
+//                 <div className={user.age < 18 ? "green" : "red"} key={user.id}>
+//                   <p>Name: {user.name}</p>
+//                   <p>Age: {user.age}</p>
+//                   <button onClick={()=> this.props.DeleteUser(user.id)}>Delete</button>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
 
+const DisplayInfor = (props) => {
+  const { listUser } = props;
+  return (
+    <div className="display-infor">
+      {true && (
+        <div>
+          {listUser.map((user) => {
+            return (
+              <div className={user.age < 18 ? "green" : "red"} key={user.id}>
+                <p>Name: {user.name}</p>
+                <p>Age: {user.age}</p>
+                <button onClick={() => props.DeleteUser(user.id)}>
+                  Delete
+                </button>
+                <hr />
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
 
-  componentDidMount() {
-      console.log("Mounted");
-  }
-  componentDidUpdate(prevProps, prevState) {
-      console.log("Update",this.props,prevProps);
-      if(this.props.listUser !== prevProps.listUser){
-        if(this.props.listUser.length === 0){
-          alert("List User is empty");
-        }
-      }
-  }
-
-  render() {
-    const { listUser } = this.props;
-    return (
-      <div className="display-infor">
-        <img src={logo} className="App-logo" alt="logo" />
-        <br/>
-        <button
-          onClick={() => this.setState({ showList: !this.state.showList })}
-        >
-          {this.state.showList ? "Hide List" : "Show List"}
-        </button>
-        {this.state.showList && (
-          <div>
-            {listUser.map((user) => {
-              return (
-                <div className={user.age < 18 ? "green" : "red"} key={user.id}>
-                  <p>Name: {user.name}</p>
-                  <p>Age: {user.age}</p>
-                  <button onClick={()=> this.props.DeleteUser(user.id)}>Delete</button>
-                  <hr />
-                </div>
-              );
-            })}
-          </div>
-        )}
-      </div>
-    );
-  }
-}
 export default DisplayInfor;
