8f73f272a5d3a877716b5bcc1611137558bcdb42
khang <khangcaopham2003@gmail.com>
2024-08-03 00:42:58 +0700

    nested Routes learn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./App.scss";
 import Header from "./components/Header/Header";
 
 const App = () => {
   return (
-    <div className="app">
-      <Header />
-
+    <div className="app-container">
+      <div className="header-container">
+        <Header />
+      </div>
+      <div className="main-container">
+        <div className="sidenav-container"></div>
+        <div className="app-content">
+          <Outlet />
+        </div>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -14,11 +14,11 @@ root.render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/users" element={<User />} />
-          <Route path="/admins" element={<Admin />} />
+          <Route path="/" element={<App />}>
+            <Route path="/users" element={<User />} />
+            <Route path="/admins" element={<Admin />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
