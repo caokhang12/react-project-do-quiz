87f6b1fdc0b0c75003d606d558d9b9ab81cab7e2
khang <khangcaopham2003@gmail.com>
2024-08-03 17:24:05 +0700

    Index route learn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary bg-light">
       <Container>
         <Navbar.Brand href="#home">CaoKhang</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -26,9 +26,9 @@ const Header = () => {
             <Nav.Link href="/admins">Admin</Nav.Link> */}
           </Nav>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+            <NavDropdown.Item>Log in</NavDropdown.Item>
+            <NavDropdown.Item>Log out</NavDropdown.Item>
+            <NavDropdown.Item>Profile</NavDropdown.Item>
           </NavDropdown>
           <Nav></Nav>
         </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = (props) => {
+    return (
+        <div className="homepage-container">
+            <h1>HomePage</h1>
+        </div>
+    );
+}
+
+export default HomePage
